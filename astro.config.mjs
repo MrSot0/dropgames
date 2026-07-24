897ef93876c83c3bfb54603842b4ed9c9618d31f
Jesus Soto <96711801+MrSot0@users.noreply.github.com>
2026-07-24 19:29:19 -0400

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,7 @@ export default defineConfig({
   
   server: {
     port: 4321
-  }
+  },
+
+  integrations: [sitemap()]
 });
